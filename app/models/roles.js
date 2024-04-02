3787023595e9aa952afdeb53173f4512b979e381
GwendalLourdel <gwendal.lourdel@oclock.school>
2024-04-02 15:17:50 +0200

    ✨feature: create model roles

---
+++ app/models/roles.js
@@ -0,0 +1,25 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
+
+class Roles extends Model {}
+
+Roles.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+
+  name: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    unique: true,
+  },
+
+}, {
+  sequelize,
+  tableName: 'roles',
+});
+
+export default Roles;
