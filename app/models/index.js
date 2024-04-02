4ede33248d8fc4cdfa8cb2c93e7f98efc88e27d2
GwendalLourdel <gwendal.lourdel@oclock.school>
2024-04-02 16:26:25 +0200

    ✨feature: creation of the 4 many-to-many relations between tables

--- app/models/index.js
+++ app/models/index.js
@@ -8,12 +8,56 @@ import Tags from './tags.js';
 import UserBookmark from './userBookmark.js';
 import Users from './users.js';
 import sequelize from '../config/client.js';
+import MemeBookmark from './userBookmark.js';
 
-Memes.hasMany(MemeComment, {
-  as: 'Comments',
-  foreignKey: 'memeId',
+Memes.belongsToMany(Users, {
+  as: 'CommentedBy',
+  through: MemeComment,
+  foreignKey: 'memesId',
+  otherKey: 'usersId',
 });
-MemeComment.belongsTo(Users, {
-  as: 'User',
-  foreignKey: 'userId',
+Users.belongsToMany(Memes, {
+  as: 'Comments',
+  through: MemeComment,
+  foreignKey: 'usersId',
+  otherKey: 'memesId',
+});
+
+Users.belongsToMany(Memes, {
+  as: 'Likes',
+  through: MemeLike,
+  foreignKey: 'usersId',
+  otherKey: 'memesId',
+});
+Memes.belongsToMany(Users, {
+  as: 'LikedBy',
+  through: MemeLike,
+  foreignKey: 'memesId',
+  otherKey: 'usersId',
+});
+
+Users.belongsToMany(Memes, {
+  as: 'Bookmarks',
+  through: MemeBookmark,
+  foreignKey: 'usersId',
+  otherKey: 'memesId',
+});
+Memes.belongsToMany(Users, {
+  as: 'BookmarkedBy',
+  through: MemeBookmark,
+  foreignKey: 'memesId',
+  otherKey: 'usersId',
+});
+
+Tags.belongsToMany(Memes, {
+  as: 'Tags',
+  through: MemeTag,
+  foreignKey: 'TagsId',
+  otherKey: 'memesId',
+});
+Memes.belongsToMany(Tags, {
+  as: 'Memes',
+  through: MemeTag,
+  foreignKey: 'memesId',
+  otherKey: 'TagsId',
 });
