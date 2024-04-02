a5ab54109edfa9a484a5f461bc767187cf0267f2
GwendalLourdel <gwendal.lourdel@oclock.school>
2024-04-02 13:33:34 +0200

    ✨feature: finalisation index.app.js

--- app/index.app.js
+++ app/index.app.js
@@ -1,8 +1,19 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 import router from './routers/index.router.js';
 
 const app = express();
+
+app.use(helmet());
+app.use(cors());
+
+app.use(cookieParser());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
 
