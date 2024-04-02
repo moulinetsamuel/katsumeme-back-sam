efdfc127ff3d6a75d2694fed3e3e738e27273c31
GwendalLourdel <gwendal.lourdel@oclock.school>
2024-04-02 15:17:36 +0200

    ✨feature: create model refresh_tokens

---
+++ app/models/refreshToken.js
@@ -0,0 +1,30 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
+
+class RefreshTokens extends Model {}
+
+RefreshTokens.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+
+  token: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    unique: true,
+  },
+
+  expiresAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+
+}, {
+  sequelize,
+  tableName: 'refresh_tokens',
+});
+
+export default RefreshTokens;
