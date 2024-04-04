4058a5e74e481796b13a295c701a3873d81d0175
GwendalLourdel <gwendal.lourdel@oclock.school>
2024-04-04 14:10:15 +0200

    ✨feature: index router update

--- app/routers/index.router.js
+++ app/routers/index.router.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import testController from '../controllers/test.controller.js';
+import apiRouter from './api.router/index.api.router.js';
 
 const router = express.Router();
 
 router.get('/test', testController);
+router.use('/api', apiRouter);
 
 export default router;
