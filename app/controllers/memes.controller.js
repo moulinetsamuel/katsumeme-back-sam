3cd27d46b994402fd667d72a89df9551f703717a
GwendalLourdel <gwendal.lourdel@oclock.school>
2024-04-05 12:39:59 +0200

    ✨feature: completion path GET for memes homepage

--- app/controllers/memes.controller.js
+++ app/controllers/memes.controller.js
@@ -1,13 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
+const pretty = (obj) => JSON.stringify(obj, null, 2);
+const cpretty = (obj) => console.log(pretty(obj));
 
 const memesController = {
   async getAll(req, res) {
-    const { page, limit } = req.query;
+    const page = Number(req.query.page);
+    const limit = Number(req.query.limit);
     const offset = (page - 1) * limit;
 
-    const allMemes = await prisma.meme.findMany({
+    const allMemesWithTagsAndLikes = await prisma.meme.findMany({
       take: limit,
       skip: offset,
       orderBy: {
@@ -16,42 +19,86 @@ const memesController = {
       include: {
         tags: {
           select: {
-            name: true,
+            tags: {
+              select: {
+                name: true,
+              },
+            },
           },
         },
         author: {
           select: {
             nickname: true,
-            avatarUrl: true,
+            avatar_url: true,
           },
         },
-        likedBy: true,
-        bookmarkedBy: true,
+        _count: {
+          select: {
+            liked_by: {
+              where: { like: true },
+            },
+          },
+        },
       },
     });
-    // const completedMemes = allMemes.map((meme) => {
-    //   // const numberOfLikes = meme.likedBy.filter(like => like.like === true).length;
-    //   // const numberOfDislikes = meme.likedBy.filter);
-    //   const userLiked = meme.likedBy.some((like) => like.userId === req.user.id);
-    //   const isBookmarked = meme.bookmarkedBy.some((bookmark) => bookmark.userId === req.user.id);
 
-    //   return {
-    //     title: meme.title,
-    //     tags: meme.tags.map((tag) => tag.name),
-    //     imageUrl: meme.imageUrl,
-    //     author: {
-    //       nickname: meme.author.nickname,
-    //       avatarUrl: meme.author.avatarUrl,
-    //     },
-    //     createdAt: meme.createdAt,
-    //     // numberOfLikes,
-    //     // numberOfDislikes,
-    //     userLiked,
-    //     isBookmarked,
-    //   };
-    // });
+    const countDislikes = await prisma.meme.findMany({
+      take: limit,
+      skip: offset,
+      orderBy: {
+        id: 'desc',
+      },
+      include: {
+        _count: {
+          select: {
+            liked_by: {
+              where: { like: false },
+            },
+          },
+        },
+      },
+    });
 
-    res.status(200).send(allMemes);
+    const dislikeNumber = countDislikes.map((meme) => meme._count);
+
+    const allMemesWithTagsLikesAndDislikes = allMemesWithTagsAndLikes.map((meme, index) => ({
+      ...meme,
+      dislikeCount: dislikeNumber[index],
+    }));
+
+    const activeUserId = Number(req.query.user_id);
+
+    const memesCompleted = await Promise.all(allMemesWithTagsLikesAndDislikes.map(async (meme) => {
+      const memeId = meme.id;
+
+      // Vérifiez si le mème en question est bookmarké par l'utilisateur actif
+      const memeIsBookmarked = await prisma.user_has_bookmark.findFirst({
+        where: {
+          user_id: activeUserId, // L'ID de l'utilisateur actif
+          meme_id: memeId, // L'ID du mème
+        },
+      });
+
+      // Vérifiez si l'utilisateur a liké ce mème
+      const likedByUser = await prisma.meme_has_like.findFirst({
+        where: {
+          user_id: activeUserId, // L'ID de l'utilisateur actif
+          meme_id: memeId, // L'ID du mème
+        },
+      });
+
+      return {
+        ...meme,
+        isBookmarked: !!memeIsBookmarked, // Convertit en booléen
+        likedByUser: likedByUser ? likedByUser.like : null, // null si l'utilisateur n'a pas encore donné de valeur
+      };
+    }));
+
+    // cpretty(allMemesWithTagsLikesAndDislikes);
+    // cpretty(allCompleteMemes);
+
+    cpretty(memesCompleted);
+    res.status(200).send(memesCompleted);
   },
 };
 
