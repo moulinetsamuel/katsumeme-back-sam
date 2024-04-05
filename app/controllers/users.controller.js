5097399e854785080f5e1b2a8706ae2792b3b011
GwendalLourdel <gwendal.lourdel@oclock.school>
2024-04-05 17:40:28 +0200

    ✨feature: user creation controller update

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -1,10 +1,27 @@
 import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
 export default {
   async signup(req, res) {
-    const newUser = await prisma.user.create(req.body);
-    res.status(201).json(newUser);
+    const {
+      password, firstname, lastname, nickname, email,
+    } = req.body;
+    const hashedPassword = await bcrypt.hash(password, parseInt(process.env.NB_OF_SALT_ROUNDs, 10));
+
+    const newUser = await prisma.user.create({
+      data: {
+        firstname,
+        lastname,
+        nickname,
+        email,
+        password: hashedPassword,
+      },
+
+    });
+    console.log(newUser);
+
+    res.status(201).json({ message: 'Your account has been created' });
   },
 };
