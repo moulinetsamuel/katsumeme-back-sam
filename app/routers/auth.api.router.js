5036479ebf4a8c61a5f97e9445828608caa4f838
moulinetsamuel <samuel.moulinet@oclock.school>
2024-04-05 12:07:50 +0200

    ✨feature: add validation zod schemas in router

--- app/routers/auth.api.router.js
+++ app/routers/auth.api.router.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import authController from '../controllers/auth.controller.js';
 import tryCatcher from '../middlewares/tryCatcher.js';
+import zodValidator from '../middlewares/zod.validation.schema.js';
+import loginSchema from '../schemas/auth.schema/post.login.js';
 
 const router = express.Router();
 
-router.post('/login', tryCatcher(authController.login));
+router.post('/login', zodValidator(loginSchema), tryCatcher(authController.login));
+
+export default router;
