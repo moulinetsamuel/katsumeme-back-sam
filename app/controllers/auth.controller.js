2ef649686c25b3e69d4727ba6a5ec893151addfa
moulinetsamuel <samuel.moulinet@oclock.school>
2024-04-05 12:08:03 +0200

    ✨feature: controller for login

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,10 +1,18 @@
-import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcrypt';
-
-const prisma = new PrismaClient();
+import authenticate from '../utils/authenticate.js';
+import generateTokens from '../utils/generateTokens.js';
 
 export default {
   async login(req, res) {
     const { email, password } = req.body;
+
+    const user = await authenticate(email, password);
+
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const { accessToken, refreshToken } = await generateTokens(user);
+
+    return res.status(200).json({ accessToken, refreshToken });
   },
 };
