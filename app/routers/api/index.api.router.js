0fd4d5b442efb3b051d712993ec4b449012b8647
GwendalLourdel <gwendal.lourdel@oclock.school>
2024-04-05 15:50:21 +0200

    ✨feature: user creation index router update

--- app/routers/api/index.api.router.js
+++ app/routers/api/index.api.router.js
@@ -2,11 +2,13 @@ import express from 'express';
 import testRouter from './test.api.router.js';
 import authRouter from './auth.api.router.js';
 import memesRouter from './memes.api.router.js';
+import usersRouter from './users.api.router.js';
 
 const router = express.Router();
 
 router.use('/test', testRouter);
 router.use('/auth', authRouter);
 router.use('/memes', memesRouter);
+router.use('/users', usersRouter);
 
 export default router;
