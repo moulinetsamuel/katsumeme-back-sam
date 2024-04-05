8e93e3af51d45c66b94803db535aca69192b678e
GwendalLourdel <gwendal.lourdel@oclock.school>
2024-04-05 15:50:59 +0200

    ✨feature: user creation api router

---
+++ app/routers/api/users.api.router.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import usersController from '../../controllers/users.controller.js';
+import tryCatcher from '../../middlewares/tryCatcher.js';
+import zodValidator from '../../middlewares/zod.validation.schema.js';
+import signupSchema from '../../schemas/users.schema/post.signup.js';
+
+const router = express.Router();
+
+router.post('/signup', zodValidator(signupSchema), tryCatcher(usersController.signup));
+
+export default router;
