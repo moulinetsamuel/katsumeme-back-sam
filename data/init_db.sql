bfc1bde1038178a6f5ebc38c4e0bafc191b384ec
moulinetsamuel <samuel.moulinet@oclock.school>
2024-04-08 00:26:23 +0200

    ✨feature: Update JWT_EXPIRES_IN value and create database and user in init_db.sql

--- data/init_db.sql
+++ data/init_db.sql
@@ -1,5 +1,5 @@
--- DROP DATABASE IF EXISTS katsumeme;
--- DROP USER IF EXISTS katsumeme;
+DROP DATABASE IF EXISTS katsumeme;
+DROP USER IF EXISTS katsumeme;
 CREATE USER katsumeme WITH PASSWORD 'katsumeme';
--- ALTER USER katsumeme CREATEDB;
+ALTER USER katsumeme CREATEDB;
 CREATE DATABASE katsumeme OWNER katsumeme;
