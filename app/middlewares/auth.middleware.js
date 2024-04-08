79dd8f742932cbffc705f1fbfc236e476eb891fe
moulinetsamuel <samuel.moulinet@oclock.school>
2024-04-08 16:17:37 +0200

    ✨feature: Refactor auth.middleware.js and zod.validation.schema.js

--- app/middlewares/auth.middleware.js
+++ app/middlewares/auth.middleware.js
@@ -1,27 +1,28 @@
 import jwt from 'jsonwebtoken';
 import { PrismaClient } from '@prisma/client';
+import { AuthError } from '../error/api.error.js';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  errorFormat: 'minimal',
+});
 
 export default async (req, res, next) => {
   try {
     const authorizationHeader = req.headers.authorization;
 
-    if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
-      const error = new Error('Invalid authorization header');
-      error.status = 401;
-      throw error;
+    if (!authorizationHeader || !authorizationHeader.startsWith('Bearer')) {
+      throw new AuthError('Authorization header not found', 401);
     }
 
     const accessToken = authorizationHeader.split(' ')[1];
 
     if (!accessToken) {
-      throw new Error('Access token not found', 401);
+      throw new AuthError('Access token not found', 401);
     }
 
     const decoded = jwt.verify(accessToken, process.env.JWT_SECRET);
 
-    const userId = decoded.sub;
+    const userId = Number(decoded.sub);
 
     const user = await prisma.user.findUnique({
       where: {
@@ -30,15 +31,16 @@ export default async (req, res, next) => {
     });
 
     if (!user) {
-      const error = new Error('User not found');
-      error.status = 401;
-      throw error;
+      throw new AuthError('User not found', 401);
     }
 
     req.user = user;
 
     next();
   } catch (error) {
+    if (error.name === 'TokenExpiredError' || error.name === 'JsonWebTokenError') {
+      error.status = 401;
+    }
     next(error);
   }
 };
--- app/middlewares/zod.validation.schema.js
+++ app/middlewares/zod.validation.schema.js
@@ -1,8 +1,12 @@
+import { fromZodError } from 'zod-validation-error';
+
 export default (schema) => async (req, res, next) => {
   try {
     await schema.parseAsync(req.body);
     next();
   } catch (error) {
-    next(error);
+    const zodError = fromZodError(error);
+    zodError.status = 400;
+    next(zodError);
   }
 };
