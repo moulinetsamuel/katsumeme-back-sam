a53f9f52ee751632da79923bfe9cb52001b57958
moulinetsamuel <samuel.moulinet@oclock.school>
2024-04-08 16:17:54 +0200

    ✨feature: Update error middleware and test files

--- app/middlewares/error.middleware.js
+++ app/middlewares/error.middleware.js
@@ -1,23 +1,27 @@
 // eslint-disable-next-line no-unused-vars
-export default (err, _, res, __) => {
+export default (err, req, res, next) => {
   // 0 - Simple message
-  // 1 - Message without error
-  // 2 - All information
-  const debugLevel = 0;
+  // 1 - Entire error and status
+  const debugLevel = 1;
   let message = {};
 
   switch (debugLevel) {
     case 0:
-      message = { message: err.message };
-      if (err.name === 'SequelizeDatabaseError') {
-        message = { message: 'Database Error' };
+      console.log(err.name);
+      if (err.name === 'AuthError') {
+        message = { message: 'Unauthorized' };
+      } else if (err.name === 'TokenExpiredError' || err.name === 'JsonWebTokenError') {
+        message = { message: 'Invalid token' };
+      } else if (err.name.startsWith('PrismaClient')) {
+        message = { message: 'Database error' };
+      } else if (err.name === 'ZodValidationError') {
+        message = { message: err };
+      } else {
+        message = { message: 'Je suis pas gérée' };
       }
       break;
     case 1:
-      message = { message: err.message };
-      break;
-    case 2:
-      message = { message: err.message, error: err };
+      message = { message: err.message.replace(/\n/g, ''), error: err };
       console.log(err);
       break;
     default:
