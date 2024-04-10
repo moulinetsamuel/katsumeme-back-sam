6cc13e5b29cdab9ad23d0fa5fa25f9d8af52cadd
moulinetsamuel <samuel.moulinet@oclock.school>
2024-04-10 14:58:56 +0200

    ✨feature: Refactor multer middleware import in memes.api.router.js

--- app/routers/api/memes.api.router.js
+++ app/routers/api/memes.api.router.js
@@ -1,19 +1,14 @@
 import express from 'express';
 import memesController from '../../controllers/memes.controller.js';
 import tryCatcher from '../../middlewares/tryCatcher.js';
-import { memeUpload } from '../../middlewares/multer.middleware.js';
+import upload from '../../middlewares/multer.middleware.js';
 import zodValidationSchema from '../../middlewares/zod.validation.schema.js';
 import postMemes from '../../schemas/memes.schema/post.memes.js';
 import auth from '../../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
-// router.use(multerUpload);
 router.get('/', tryCatcher(memesController.getAll));
-router.post('/', auth, memeUpload.single('meme'), zodValidationSchema(postMemes), tryCatcher(memesController.uploadMeme));
-
-// router.get('/:id', tryCatcher(memesController.getOne));
-
-// router.delete('/:id', tryCatcher(memesController.delete));
+router.post('/', auth, upload.single('meme'), zodValidationSchema(postMemes), tryCatcher(memesController.uploadMeme));
 
 export default router;
