a5f1978193119b6cc548231e465e234807531ff8
GwendalLourdel <gwendal.lourdel@oclock.school>
2024-04-11 16:31:32 +0200

    ✨feature: like/dislike/bookmark router pathing

--- app/routers/api/toggle.api.router.js
+++ app/routers/api/toggle.api.router.js
@@ -6,7 +6,7 @@ import tryCatcher from '../../middlewares/tryCatcher.js';
 const router = express.Router();
 
 router.get('/like/meme/:id', auth, tryCatcher(toggleController.toggleLike));
-// router.get('/dislike/meme', auth, tryCatcher(toggleController.toggleDislike));
-// router.get('/bookmark/meme', auth, tryCatcher(toggleController.toggleBookmark));
+router.get('/dislike/meme/:id', auth, tryCatcher(toggleController.toggleDislike));
+router.get('/bookmark/meme/:id', auth, tryCatcher(toggleController.toggleBookmark));
 
 export default router;
