730d596e1f6b24f0c35cf3345bd3b63cefe355c7
GwendalLourdel <gwendal.lourdel@oclock.school>
2024-04-11 16:31:53 +0200

    ✨feature: like/dislike/bookmark pathing

--- app/controllers/toggle.controller.js
+++ app/controllers/toggle.controller.js
@@ -4,34 +4,135 @@ const prisma = new PrismaClient();
 
 const toggleController = {
   toggleLike: async (req, res) => {
-    const memeId = req.params.id;
-    const userId = req.user.id;
+    const memeId = Number(req.params.id);
+    const userId = Number(req.user.id);
 
-    const existingLike = await prisma.meme_has_like.findUnique({
+    const existingLike = await prisma.meme_has_like.findFirst({
       where: {
         user_id: userId,
-        meme_id: Number(memeId),
+        meme_id: memeId,
       },
     });
 
     if (existingLike) {
-      await prisma.meme_has_like.delete({
-        where: {
-          id: existingLike.id,
-        },
-      });
+      if (existingLike.like === true) {
+        await prisma.meme_has_like.delete({
+          where: {
+            user_id_meme_id: {
+              user_id: userId,
+              meme_id: memeId,
+            },
+          },
+        });
+        res.status(200).json({ message: 'Like successfully removed' });
+      } else {
+        await prisma.meme_has_like.update({
+          where: {
+            user_id_meme_id: {
+              user_id: userId,
+              meme_id: memeId,
+            },
+          },
+          data: {
+            like: true,
+          },
+        });
 
-      res.status(200).json({ message: 'Like successfully removed' });
+        res.status(200).json({ message: 'Like successfully updated' });
+      }
     } else {
       await prisma.meme_has_like.create({
         data: {
           like: true,
           user_id: userId,
-          meme_id: Number(memeId),
+          meme_id: memeId,
         },
       });
 
       res.status(200).json({ message: 'Like successfully added' });
+    }
+  },
+
+  toggleDislike: async (req, res) => {
+    const memeId = Number(req.params.id);
+    const userId = Number(req.user.id);
+
+    const existingLike = await prisma.meme_has_like.findFirst({
+      where: {
+        user_id: userId,
+        meme_id: memeId,
+      },
+    });
+
+    if (existingLike) {
+      if (existingLike.like === false) {
+        await prisma.meme_has_like.delete({
+          where: {
+            user_id_meme_id: {
+              user_id: userId,
+              meme_id: memeId,
+            },
+          },
+        });
+        res.status(200).json({ message: 'Dislike successfully removed' });
+      } else {
+        await prisma.meme_has_like.update({
+          where: {
+            user_id_meme_id: {
+              user_id: userId,
+              meme_id: memeId,
+            },
+          },
+          data: {
+            like: false,
+          },
+        });
+
+        res.status(200).json({ message: 'Dislike successfully updated' });
+      }
+    } else {
+      await prisma.meme_has_like.create({
+        data: {
+          like: false,
+          user_id: userId,
+          meme_id: memeId,
+        },
+      });
+
+      res.status(200).json({ message: 'Dislike successfully added' });
+    }
+  },
+
+  toggleBookmark: async (req, res) => {
+    const memeId = Number(req.params.id);
+    const userId = Number(req.user.id);
+
+    const existingBookmark = await prisma.user_has_bookmark.findFirst({
+      where: {
+        user_id: userId,
+        meme_id: memeId,
+      },
+    });
+    if (existingBookmark) {
+      await prisma.user_has_bookmark.delete({
+        where: {
+          user_id_meme_id: {
+            user_id: userId,
+            meme_id: memeId,
+          },
+        },
+      });
+
+      res.status(200).json({ message: 'Bookmark successfully removed' });
+    } else {
+      await prisma.user_has_bookmark.create({
+        data: {
+          user_id: userId,
+          meme_id: memeId,
+        },
+      });
+
+      res.status(200).json({ message: 'Bookmark successfully added' });
     }
   },
 };
