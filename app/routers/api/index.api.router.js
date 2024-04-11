dcbe8d63c3bf822107c08033daebe5b06df7783f
GwendalLourdel <gwendal.lourdel@oclock.school>
2024-04-11 13:03:40 +0200

    ✨feature: include profil path in router

--- app/routers/api/index.api.router.js
+++ app/routers/api/index.api.router.js
@@ -3,6 +3,7 @@ import testRouter from './test.api.router.js';
 import authRouter from './auth.api.router.js';
 import memesRouter from './memes.api.router.js';
 import usersRouter from './users.api.router.js';
+import profilRouter from './profil.api.router.js';
 import auth from '../../middlewares/auth.middleware.js';
 
 const router = express.Router();
@@ -11,5 +12,6 @@ router.use('/test', auth, testRouter);
 router.use('/auth', authRouter);
 router.use('/memes', memesRouter);
 router.use('/users', usersRouter);
+router.use('/profil', auth, profilRouter);
 
 export default router;
