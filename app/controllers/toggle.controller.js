8988ade43178d33c012e3e47182dcf377f87a359
GwendalLourdel <gwendal.lourdel@oclock.school>
2024-04-11 11:41:52 +0200

    ✨feature: toggleController creation

---
+++ app/controllers/toggle.controller.js
@@ -0,0 +1,39 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const toggleController = {
+  toggleLike: async (req, res) => {
+    const memeId = req.params.id;
+    const userId = req.user.id;
+
+    const existingLike = await prisma.meme_has_like.findFirst({
+      where: {
+        user_id: userId,
+        meme_id: Number(memeId),
+      },
+    });
+
+    if (existingLike) {
+      await prisma.meme_has_like.delete({
+        where: {
+          id: existingLike.id,
+        },
+      });
+
+      res.status(200).json({ message: 'Like successfully removed' });
+    } else {
+      await prisma.meme_has_like.create({
+        data: {
+          like: true,
+          user_id: userId,
+          meme_id: Number(memeId),
+        },
+      });
+
+      res.status(200).json({ message: 'Like successfully added' });
+    }
+  },
+};
+
+export default toggleController;
