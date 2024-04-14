df10f313221fc222ce364a7e67b08ce29217e744
moulinetsamuel <samuel.moulinet@oclock.school>
2024-04-15 01:07:16 +0200

    ✨feature: Update static file path in index.app.js and multer.middleware.js

--- app/middlewares/multer.middleware.js
+++ app/middlewares/multer.middleware.js
@@ -2,7 +2,7 @@ import multer from 'multer';
 
 const memeStorage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, './public/upload/memes');
+    cb(null, './app/upload/memes');
   },
 
   filename(req, file, cb) {
