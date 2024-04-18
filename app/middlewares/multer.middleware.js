47a9ce85ef5a62f326411d42d680fc3f65942e1b
moulinetsamuel <samuel.moulinet@oclock.school>
2024-04-18 11:06:38 +0200

    test prod

--- app/middlewares/multer.middleware.js
+++ app/middlewares/multer.middleware.js
@@ -11,16 +11,6 @@ const memeStorage = multer.diskStorage({
   },
 });
 
-// const avatarStorage = multer.diskStorage({
-//   destination(req, file, cb) {
-//     cb(null, './public/upload/avatars');
-//   },
-//   filename(req, file, cb) {
-//     const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}`;
-//     cb(null, `avatar-${uniqueSuffix}.png`);
-//   },
-// });
-
 const upload = multer({ storage: memeStorage });
 
 export default upload;
