9af23a666a0e3d5f6df9e5b951a288e95ce65e7a
moulinetsamuel <samuel.moulinet@oclock.school>
2024-04-18 02:12:48 +0200

    Add rate limit policy to the express app

--- app/index.app.js
+++ app/index.app.js
@@ -2,10 +2,20 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import rateLimit from 'express-rate-limit';
 import router from './routers/index.router.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
+
+// Add rate limit policy
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100000, // Limit each IP to 100K requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+app.use(limiter);
 
 app.use(cors());
 
