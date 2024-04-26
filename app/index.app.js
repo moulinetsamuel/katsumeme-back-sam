e4f9f385ef184479e453cdd9216b6fb6ae17d475
moulinetsamuel <samuel.moulinet@oclock.school>
2024-04-26 23:13:47 +0200

    Update express.json and express.urlencoded options in index.app.js

--- app/index.app.js
+++ app/index.app.js
@@ -36,8 +36,8 @@ app.use(cors(corsOptions));
 
 app.use(cookieParser());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use(express.static('public'));
 
